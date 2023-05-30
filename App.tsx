b2f912ee617f4b9c6a41f4e0dea9adaeec0812c9
dexiouz <dexiouz@gmail.com>
2023-05-30 19:08:09 +0100

    - set up jumio for android now works

--- App.tsx
+++ App.tsx
@@ -1,118 +1,113 @@
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
- *
- * @format
+ * @flow
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, {Component, useState} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
+  AppRegistry,
+  Button,
+  Platform,
   StyleSheet,
-  Text,
-  useColorScheme,
   View,
+  NativeModules,
+  NativeEventEmitter,
+  TextInput,
 } from 'react-native';
+import {LogBox} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+LogBox.ignoreLogs(['new NativeEventEmitter']);
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+const {JumioMobileSDK} = NativeModules;
+const token =
+  'eyJhbGciOiJIUzUxMiIsInppcCI6IkdaSVAifQ.H4sIAAAAAAAA_5XLuw1CMQyF4V1SYykPJ3HoKChokRjAju0BAAkkxO7k3g3ojn595xPsfXqGY0iNasoNscZRwiHwnBddPWZNolVgUJyAUXCt3KAQd8vYGIdsfMeaCI3VAUdMgEQdpE-DTp5mZeqcbeGX2z98Xs2Xvj3sfr7lLexv19hQRgIthQHVFcRzAXZzM6smzOH7AxDyMEDhAAAA.cKYtgrcL4OYvmvV_A23vNc7plhIJ15GJymaMP6xdX9hlo19S6Oo0BhO8jlp8J1LfiwYvbBo4Z3ZFUZp2G_FMAw';
+const DATACENTER = 'EU';
 
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+// Jumio SDK
+const startJumio = authorizationToken => {
+  JumioMobileSDK.initialize(authorizationToken, DATACENTER);
+
+  // Setup iOS customizations
+  //    JumioMobileSDK.setupCustomizations(
+  //        {
+  //            loadingCircleIcon: "#000000",
+  //            loadingCirclePlain: "#000000",
+  //            loadingCircleGradientStart: "#000000",
+  //            loadingCircleGradientEnd: "#000000",
+  //            loadingErrorCircleGradientStart: "#000000",
+  //            loadingErrorCircleGradientEnd: "#000000",
+  //            primaryButtonBackground: {"light": "#FFC0CB", "dark": "#FF1493"}
+  //        }
+  //    );
+
+  JumioMobileSDK.start();
+};
+
+const isDeviceRooted = async () => {
+  const isRooted = await JumioMobileSDK.isRooted();
+  console.warn('Device is rooted: ' + isRooted);
+};
+
+// Callbacks - (Data is displayed as a warning for demo purposes)
+const emitterJumio = new NativeEventEmitter(JumioMobileSDK);
+emitterJumio.addListener('EventResult', EventResult =>
+  console.warn('EventResult: ' + JSON.stringify(EventResult)),
+);
+emitterJumio.addListener('EventError', EventError =>
+  console.warn('EventError: ' + JSON.stringify(EventError)),
+);
+
+export default class DemoApp extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <AuthTokenInput />
+      </View>
+    );
+  }
+}
+
+const AuthTokenInput = () => {
+  const [authorizationToken, setAuthorizationToken] = useState(token);
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View>
+      <TextInput
+        style={styles.input}
+        placeholder="Authorization token"
+        placeholderTextColor="#000"
+        returnKeyType="done"
+        onChangeText={text => setAuthorizationToken(text)}
+        value={authorizationToken}
+      />
+      <Button title="Start" onPress={() => startJumio(authorizationToken)} />
     </View>
   );
-}
-
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
+};
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
   },
-  highlight: {
-    fontWeight: '700',
+  input: {
+    width: 240,
+    height: 40,
+    marginBottom: 20,
+    borderWidth: 1,
+    color: 'black',
   },
 });
-
-export default App;
